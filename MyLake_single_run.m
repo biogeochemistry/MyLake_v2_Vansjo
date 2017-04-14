e8acfe03e57ccbac1aa87e3661abc74364b5d126
pycckuu <is.markelov@gmail.com>
2017-04-14 15:24:09 -0400

    calibrated values of parameters

--- MyLake_single_run.m
+++ MyLake_single_run.m
@@ -14,21 +14,31 @@ big_inputs = cell(1,no_runs);   % collects the inputs
 
 % x = [0.00365275111636845, 0.444888225060776, 6.21096051969837, 1.98304729966760, 0.00423770963447693, 0.585402600080357, 4.17074391909280, 1.84803487767737, 0.0420205265067727, 84.9948291324757, 30];
 
-x = [0.0957, 0.8462, 12.2918, 0.4898, 0.1904, 1.3158, 9.7200, 1.4780, 0.02, 10.0220, 40.9828];
-% x = [0.0957, 0.8462, 12.2918, 0.4898, 0.1904, 1.3158, 9.7200, 1.4780, 0.5   , 820.0220, 40.9828];
+% x = [0.0957, 0.8462, 12.2918, 0.4898, 0.1904, 1.3158, 9.7200, 1.4780, 0.02, 10.0220, 40.9828];
+% x = [0.4958, 0.1998, 1.1923, 1.4176, 0.1125, 0.2737, 1.3935, 0.8685, 0.6889, 259.7042, 40.3020];
+% x = [0.4958, 0.1054, 1.3664, 1.7035, 0.0698, 0.1546, 1.3697, 0.9468, 0.02, 100.7042, 24.7989];
 
-% x = [0.0957, 0.8462, 13.8622, 1.7753, 0.1904, 0.5186, 1.9203, 1.2372, 0.0926, 821.0220, 120.7667]; % RMSD 34
+% x = [245.0402e-003, 193.8201e-003, 1.3948e+000, 700.8806e-003, 76.1493e-003, 121.8722e-003, 1.3160e+000, 363.7310e-003, 77.1076e-003, 443.2972e+000, 34.9491e+000];
 
-% lake_params{39-7} = x(1); % 9     settling velocity for Chl1 a (m day-1)
-% lake_params{41-7} = x(2); % 11    loss rate (1/day) at 20 deg C
-% lake_params{42-7} = x(3); % 12    specific growth rate (1/day) at 20 deg C
-% lake_params{45-7} = x(4); % 15    Half saturation growth P level (mg/m3)
-% lake_params{48-7} = x(5); % 18    Settling velocity for Chl2 a (m day-1)
-% lake_params{49-7} = x(6);  % 19    Loss rate (1/day) at 20 deg C
-% lake_params{50-7} = x(7);  % 20    Specific growth rate (1/day) at 20 deg C
-% lake_params{51-7} = x(8);  % 21    Half saturation growth P level (mg/m3)
-lake_params{38-7} = x(9);  %   settling velocity for S (m day-1)
-sediment_params{22} = x(10);  % 38    R16 sorption of P on Fe k
+% new Lang test
+% x = [0.4958, 0.1054, 1.3664, 1.7035, 0.0698, 0.1546, 1.3697, 0.9468, 0.2, 1e-3, 24.7989];
+
+% x = [0.0643, 0.1368, 1.1846, 0.2516, 0.0570, 0.1000, 1.3924, 0.5457, 0.0590, 101.1213, 67.8637]; x'; % ecomac-2, best at the moment
+% x = [0.0643, 0.1368, 1.4647, 0.5293, 0.0570, 0.1000, 1.3339, 0.8793, 0.0590, 101.1213, 67.8637] % ecomac-2, rmsd 44.01
+x = [0.0643, 0.1368, 1.4647, 0.5293, 0.0570, 0.1000, 1.3339, 0.8793, 0.0590, 101.1213, 68.1137]; % ecomac-2 final result, RMSD 44.0151
+% x = [0.0643, 0.1800, 1.3659, 0.5293, 0.0570, 0.1000, 1.3339, 0.5457, 0.0590, 635.5627, 46.1497]; x'; % ecomac-2 rmsd 44.02
+% x = [173.8356e-003, 105.3827e-003, 1.1923e+000, 1.7035e+000, 69.8043e-003, 154.5866e-003, 1.3898e+000, 946.7929e-003, 366.6823e-003, 253.7042e+000, 19.3020e+000]; % ecomac-1, final result, 32.02 final result
+
+lake_params{40 -7} = x(1); % 9     settling velocity for Chl1 a (m day-1)
+lake_params{42 -7} = x(2); % 11    loss rate (1/day) at 20 deg C
+lake_params{43 -7} = x(3); % 12    specific growth rate (1/day) at 20 deg C
+lake_params{46 -7} = x(4); % 15    Half saturation growth P level (mg/m3)
+lake_params{49 -7} = x(5); % 18    Settling velocity for Chl2 a (m day-1)
+lake_params{50 -7} = x(6);  % 19    Loss rate (1/day) at 20 deg C
+lake_params{51 -7} = x(7);  % 20    Specific growth rate (1/day) at 20 deg C
+lake_params{52 -7} = x(8);  % 21    Half saturation growth P level (mg/m3)
+lake_params{39 -7} = x(9);  %       settling velocity for S (m day-1)
+sediment_params{22} = x(10);  % 38 R16 sorption of P on Fe k
 sediment_params{34} = x(11);  %    accel
 
 % parfor
@@ -40,7 +50,7 @@ for current_run = 1:no_runs
             m_start=[2000, 1, 1]; %
             m_stop=[2012, 12, 31]; %
         else
-            m_start=[2005, 1, 1]; %
+            m_start=[2000, 1, 1]; %
             m_stop=[2011, 12, 31]; %
         end
 
@@ -156,6 +166,42 @@ end
 % Nobs(2) = numel(dev2);
 %
 % clear dev1 dev2 Nobs % note: cannot clear within parfor
+
+% zinx=find(MyLake_results.z<4);
+% TP_mod = mean((MyLake_results.Pzt(zinx,:)+MyLake_results.PPzt(zinx,:) + MyLake_results.Chlzt(zinx,:)+MyLake_results.Czt(zinx,:)+MyLake_results.DOPzt(zinx,:)+MyLake_results.POCzt(zinx,:)+MyLake_results.DOCzt(zinx,:))', 2);
+% Chl_mod = mean((MyLake_results.Chlzt(zinx,:)+MyLake_results.Czt(zinx,:))', 2);
+% Pzt_mod = mean((MyLake_results.Pzt(zinx,:))', 2);
+% PPzt_mod = mean((MyLake_results.PPzt(zinx,:)+MyLake_results.POCzt(zinx,:))', 2);
+
+% load 'obs/store_obs/TOTP.dat' % measured
+% % load 'obs/store_obs/Cha.dat' % measured
+% load 'obs/store_obs/Cha_aquaM_march_2017.dat' % measured
+% load 'obs/store_obs/PO4.dat' % measured
+% load 'obs/store_obs/Part.dat' % measured
+
+
+% [TP_date,loc_sim, loc_obs] = (intersect(MyLake_results.days, TOTP(:,1)));
+% c_TOTP = RMSE(TP_mod(loc_sim, 1), TOTP(loc_obs, 2));
+
+
+% [TP_date,loc_sim, loc_obs] = (intersect(MyLake_results.days, Cha_aquaM_march_2017(:,1)));
+% c_Chl = RMSE(Chl_mod(loc_sim, 1), Cha_aquaM_march_2017(loc_obs, 2));
+
+
+% [TP_date,loc_sim, loc_obs] = (intersect(MyLake_results.days, PO4(:,1)));
+% c_PO4 = RMSE(Pzt_mod(loc_sim, 1), PO4(loc_obs, 2));
+
+
+% [TP_date,loc_sim, loc_obs] = (intersect(MyLake_results.days, Part(:,1)));
+% c_PP = RMSE(PPzt_mod(loc_sim, 1), Part(loc_obs, 2));
+
+
+% x'
+% res = sum([c_TOTP, c_Chl, c_PO4, c_PP])
+
+
+
+
 disp('Saving sediments profiles for the initial concentrations for the next run');
 Sediment_save_result_for_init_conc
 MyLake_save_result_for_init_conc
