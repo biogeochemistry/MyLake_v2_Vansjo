beed8ff30e46ec4a8519b917452f1a41bf5dcba2
pycckuu <is.markelov@gmail.com>
2017-05-19 10:34:31 -0400

    updated plots

--- MyLake_single_run.m
+++ MyLake_single_run.m
@@ -1,4 +1,4 @@
-for i=1:1000
+% for i=1:1000
 tic
 disp('Started at:')
 disp(datetime('now'));
@@ -55,8 +55,8 @@ for current_run = 1:no_runs
             m_start=[2000, 1, 1]; %
             m_stop=[2012, 12, 31]; %
         else
-            m_start=[2011, 1, 1]; %
-            m_stop=[2011, 12, 31]; %
+            m_start=[2000, 1, 1]; %
+            m_stop=[2013, 12, 31]; %
         end
 
     elseif current_run == 2;
@@ -161,4 +161,4 @@ save('IO/MyLakeResults.mat', 'MyLake_results', 'Sediment_results')
 disp('Finished at:')
 disp(datetime('now'));
 toc
-end
+% end
