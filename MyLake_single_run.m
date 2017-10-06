eb26308f063b65b7aa85e46811459ddb110ab2ec
pycckuu <is.markelov@gmail.com>
2017-10-07 00:36:03 +0400

    updated

--- MyLake_single_run.m
+++ MyLake_single_run.m
@@ -10,36 +10,12 @@ use_INCA = 0; % 1- MyLake will take written INCA input, either written just now 
 is_metrics = true; % print metrics in the end
 
 m_start=[2000, 1, 1]; %
-m_stop=[2013, 12, 31]; %
+m_stop=[2000, 12, 31]; %
 
-save_initial_conditions = true; % save final concentrations as initial for the next run
-file_name = 'IO/niva_res_2.mat'
+save_initial_conditions = false; % save final concentrations as initial for the next run
+file_name = 'IO/test_C.mat'
 
 [lake_params, sediment_params] = load_params();
-
-% sediment_params{62}  = 14.4/2; % 62 alfa0 bioirrigation
-% sediment_params{74}  = 0; % 74 pH algorithm
-% lake_params{19}  = 1; % POC scaling
-% lake_params{34}  = 50; % Fe3 scaling
-
-
-
-% % ecomac-2 results
-% lake_params{47} = 0.164426980537189; % 50.0000e-003  % 47     settling velocity for Chl1 a (m day-1)
-% lake_params{49} = 0.254982092942300; % 110.6689e-003  % 49    loss rate (1/day) at 20 deg C
-% lake_params{50} = 1.25000000000000; % 1.0000e+000  % 50    specific growth rate (1/day) at 20 deg C
-% lake_params{53} = 0.502268862228062; % 638.9222e-003  % 53    Half saturation growth P level (mg/m3)
-% lake_params{56} = 0.217584383024992; % 204.8121e-003  % 56    Settling velocity for Chl2 a (m day-1)
-% lake_params{57} = 0.100000000000000; % 167.6746e-003   % 57    Loss rate (1/day) at 20 deg C
-% lake_params{58} = 1.42096457634565; % 1.0985e+000   % 58    Specific growth rate (1/day) at 20 deg C
-% lake_params{59} = 0.342624466538650; % 1.5525e+000   % 59    Half saturation growth P level (mg/m3)
-% lake_params{46} = 0.120588686751271; % 53.9466e-003   % % 46  settling velocity for S (m day-1)
-% lake_params{10} = 1.27800593870456e-05; % 24.5705e-006  % 10    PAR saturation level for phytoplankton growth (mol(quanta) m-2 s-1)
-% lake_params{54} = 1.00000000000000e-05; % 75.5867e-006  % 16    PAR saturation level for phytoplankton growth (mol(quanta) m-2 s-1)
-% lake_params{12} = 0.0450000000000000; % 45.0000e-003  % 12    Optical cross_section of chlorophyll (m2 mg-1)
-% lake_params{55} = 0.0450000000000000; % 29.6431e-003  % 17    Optical cross_section of chlorophyll (m2 mg-1)
-% sediment_params{52} = 32.5; % 65.1237e+000   %    accel
-% lake_params{24} = 1.06941658550207; % 390.1162e-003   % 24    scaling factor for inflow concentration of POP (-)
 
 % Niva results RMSD = 130
 lake_params{47} = 58.3842e-003; % 50.0000e-003  % 47     settling velocity for Chl1 a (m day-1)
