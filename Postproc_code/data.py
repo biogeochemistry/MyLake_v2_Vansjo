97ce5a1928a2d902b20701bd797997edf3408442
pycckuu <is.markelov@gmail.com>
2017-10-23 16:23:37 +0200

    80 char long

--- Postproc_code/data.py
+++ Postproc_code/data.py
@@ -5,7 +5,8 @@ import matplotlib.pyplot as plt
 
 def read_data(f, h=[1, ]):
     df = pd.read_excel(f, 'WaterChemistry', header=h)
-    df['SampleDate'] = pd.to_datetime(df['SampleDate'], format='%d.%m.%Y %H:%M:%S', errors='coerce')
+    df['SampleDate'] = pd.to_datetime(
+        df['SampleDate'], format='%d.%m.%Y %H:%M:%S', errors='coerce')
     df = df.convert_objects(convert_numeric=True)
     df = df.replace('NaN', np.NaN)
     return df
@@ -15,5 +16,6 @@ def plot_depth(df, y, depth, ax=None):
     x = 'SampleDate'
     if not ax:
         ax = plt.gca()
-    ax.plot_date(df[(np.isfinite(df[y])) & (df['Depth1'] == depth)][x], df[(np.isfinite(df[y])) & (df.Depth1 == depth)][y], label=y + ' at ' + str(depth) + ' m')
-    return ax
+    ax.plot_date(df[(np.isfinite(df[y])) & (df['Depth1'] == depth)][x], df[(
+        np.isfinite(df[y])) & (df.Depth1 == depth)][y], label=y + ' at ' + str(depth) + ' m')
+    return ax  # wterr
