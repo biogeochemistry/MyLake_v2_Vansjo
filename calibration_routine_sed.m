7dc68a840a655985fb4018cb4c6dc034fe2a0e6c
pycckuu <is.markelov@gmail.com>
2018-01-03 14:31:53 +0100

    updated routine

--- calibration_routine_sed.m
+++ calibration_routine_sed.m
@@ -91,7 +91,7 @@ lake_params{41} = x(20);%    k_viv_pre
 
 % modifications:
 sediment_params{73} = 24;
-sediment_params{74} = 1;
+sediment_params{74} = 0; % pH module off, const pH = 8
 
 run_ID = 'Vansjo_Hist_M0' ; %  CALIBRATION RUN
 clim_ID = run_ID;
