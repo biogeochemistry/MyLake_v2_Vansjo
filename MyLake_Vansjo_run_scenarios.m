e97180def26c1fb3a60eb65a4f5db903a95f2572
pycckuu <is.markelov@gmail.com>
2018-02-08 12:02:58 +0100

    temperature scenarios

--- MyLake_Vansjo_run_scenarios.m
+++ MyLake_Vansjo_run_scenarios.m
@@ -1,18 +1,18 @@
 
 % scen = {'T_only_full_scen_base_historical_20y', 'P_2016_cutoff', 'P_2016_pulse', 'P_gradual_increase_2020_cutoff_to_hist', 'P_gradual_increase_2025_cutoff_to_hist', 'P_gradual_increase_2030_cutoff_to_hist', 'P_gradual_increase_2035_cutoff_to_hist', 'P_gradual_increase_2040_cutoff_to_hist', 'P_gradual_increase_2045_cutoff_to_hist', 'P_gradual_increase_2050_cutoff_to_hist', 'P_gradual_increase_2050_cutoff', 'P_gradual_increase'}
 
-% scen = {'T_only_full_scen_base_historical_20y', 'T_only_RCP4_GFDL', 'T_only_RCP4_IPSL', 'T_only_RCP8_GFDL', 'T_only_RCP8_IPSL', 'T_only_RCP45_NorESM', 'T_only_RCP85_NorESM'}
+scen = {'T_only_full_scen_base_historical_20y', 'T_only_RCP4_GFDL', 'T_only_RCP4_IPSL', 'T_only_RCP8_GFDL', 'T_only_RCP8_IPSL', 'T_only_RCP45_NorESM', 'T_only_RCP85_NorESM'}
 % start = {}
 
-scen = {'P_gradual_increase_2050_cutoff_to_0', 'P_gradual_increase_2045_cutoff_to_0', 'P_gradual_increase_2040_cutoff_to_0', 'P_gradual_increase_2035_cutoff_to_0', 'P_gradual_increase_2030_cutoff_to_0', 'P_gradual_increase_2025_cutoff_to_0', 'P_gradual_increase_2020_cutoff_to_0', 'P_gradual_increase_2015_cutoff_to_0'}
+% scen = {'P_gradual_increase_2050_cutoff_to_0', 'P_gradual_increase_2045_cutoff_to_0', 'P_gradual_increase_2040_cutoff_to_0', 'P_gradual_increase_2035_cutoff_to_0', 'P_gradual_increase_2030_cutoff_to_0', 'P_gradual_increase_2025_cutoff_to_0', 'P_gradual_increase_2020_cutoff_to_0', 'P_gradual_increase_2015_cutoff_to_0'}
 
 
 parfor s = 1:size(scen,2)
 
     % m_start=[1985, 1, 1]; %
     % m_stop=[2040, 12, 31]; %
-    m_start=[1985, 1, 1]; %
-    m_stop=[2100, 12, 31]; %
+    m_start=[2015, 1, 1]; %
+    m_stop=[2070, 12, 31]; %
 
 
     [lake_params, sediment_params] = load_params();
